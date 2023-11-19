652e2fa65270794ad827bbe7f71a4a0b89c25291
Douaekanjaa <kanjaadouae1@gmail.com>
2023-11-19 19:13:25 +0100

    cards

--- src/compenants/firstpage/firstpage.js
+++ src/compenants/firstpage/firstpage.js
@@ -9,26 +9,30 @@ export default function Firstpage() {
   const { darkMode } = useTheme();
   return (
     <div className={`fg${darkMode ? ' dark-mode' : ''}`}>
-       <Row className={`justify-content-center p-md-3 p-2 h-100 page1${darkMode ? ' dark-mode' : ''}`}>
+       <Row className={`justify-content-center mb-3 p-md-3 p-2 h-100 page1${darkMode ? ' dark-mode' : ''}`}>
       {/* Card 1 */}
-      <Col xs={12} md={6} className='my-md-3 my-1 '>
+      <Col xs={12} md={4} className='my-md-3 my-1 mx-md-3'>
         <Card className='shadow'>
           
           <Card.Body>
-            <Card.Title>Card 1</Card.Title>
+          <Card.Img  className='mx-4' variant="top" src={`${process.env.PUBLIC_URL}/images/calculator_img.png`} style={{ width: '77%', height: '290px' }}/>
+      
+            <Card.Title>TP 1</Card.Title>
             <Card.Text>
               This is a sample card. You can add any content here.
             </Card.Text>
           </Card.Body>
         </Card>
+
       </Col>
 
       {/* Card 2 */}
-      <Col xs={12} md={6}  className='my-md-3 my-1'>
+      <Col xs={12} md={4}  className='my-md-3 my-1 mx-md-3'>
         <Card className='shadow'>
          
           <Card.Body>
-            <Card.Title>Card 2</Card.Title>
+          <Card.Img  className='mx-4' variant="top" src={`${process.env.PUBLIC_URL}/images/todo_img.png`} style={{ width: '81%', height: '290px' }}/>
+            <Card.Title>TP 2</Card.Title>
             <Card.Text>
               This is another sample card with different content.
             </Card.Text>
@@ -37,27 +41,28 @@ export default function Firstpage() {
       </Col>
 
       {/* Card 3 */}
-      <Col xs={12} md={6} className='my-md-3 my-1'>
+      <Col xs={12} md={4}  className='my-md-3 mx-md-3 my-1'>
         <Card className='shadow'>
-        
+         
           <Card.Body>
-            <Card.Title>Card 3</Card.Title>
+          <Card.Img  className='mx-4' variant="top" src={`${process.env.PUBLIC_URL}/images/slider_img.png`} style={{ width: '81%', height: '290px' }}/>
+            <Card.Title>TP 3</Card.Title>
             <Card.Text>
-              Add your own content for the third card here.
+              This is another sample card with different content.
             </Card.Text>
           </Card.Body>
         </Card>
       </Col>
 
       {/* Card 4 */}
-      <Col xs={12} md={6} className='my-md-3 my-1'>
+      <Col xs={12} md={4}  className='my-md-3 my-1 mx-md-3'>
         <Card className='shadow'>
-          <Card.Body>
-          {/* <Card.Img variant="top" src="https://via.placeholder.com/150" /> */}
          
-            <Card.Title>Card 4</Card.Title>
+          <Card.Body>
+          <Card.Img  className='mx-4' variant="top" src={`${process.env.PUBLIC_URL}/images/weather_img.png`} style={{ width: '81%', height: '290px' }}/>
+            <Card.Title>TP 4</Card.Title>
             <Card.Text>
-              Customize the content for the fourth card as needed.
+              This is another sample card with different content.
             </Card.Text>
           </Card.Body>
         </Card>
